ac9c2c346874a257769024149b32328bab00f3bb
Zachary Hayward <zmhhayward@gmail.com>
2024-06-20 14:09:38 -0500

    Smoother registering. Shows how to make your development routes work if you need to avoid auth/registering in App.tsx.

--- client/components/App.tsx
+++ client/components/App.tsx
@@ -1,48 +1,25 @@
-import { useContext, createContext, useEffect, useState } from 'react'
+import { useState } from 'react'
 import { useAuth0 } from '@auth0/auth0-react'
 import { Outlet } from 'react-router-dom'
 import Footer from './Footer.tsx'
 import NavBar from './NavBar.tsx'
 import LandingPage from '../pages/LandingPage.tsx'
 import Register from '../pages/Register.tsx'
-import { getUserByAuth } from '../apis/growGrub.ts'
-import { User } from '../../models/growGrub.ts'
-
-const UserContext = createContext<User | undefined>(undefined)
-export const useUser = () => useContext(UserContext)
 
 function App() {
-  const [user, setUser] = useState<User>()
-  const [redirecting, setRedirecting] = useState(true)
-  const {isAuthenticated, getAccessTokenSilently} = useAuth0()
-  
-  useEffect(() => {
-    const getUserByToken = async () => {
-      try {
-        const token = await getAccessTokenSilently()
-        const user = await getUserByAuth(token)
-        // console.log(token) use this to grab your token for thunderclient testing
-        setUser(user)
-        setRedirecting(false)
-      } catch(error) {
-        console.error(error)
-      }
-    }
-    getUserByToken()
-  },[isAuthenticated, getAccessTokenSilently])
+  const [registered, setRegistered] = useState(false)
+  const {isAuthenticated} = useAuth0()
   
   return (
     <>
       <div className='app min-w-screen min-h-screen'>
         <NavBar />
-          {(!isAuthenticated || redirecting) ?
+          {(!isAuthenticated) ?
             <LandingPage />
-          : <>{!user ? 
-                <Register />
+          : <>{!registered ? 
+                <Register registered={registered} setRegistered={setRegistered}/>
               :
-                <UserContext.Provider value={user}>
-                  <Outlet />
-                </UserContext.Provider>
+                <Outlet />
             }</>
           }
         <Footer />
@@ -52,3 +29,20 @@ function App() {
 }
 
 export default App
+
+//Swap this:
+
+// {(!isAuthenticated) ?
+//   <LandingPage />
+// : <>{!registered ? 
+//       <Register registered={registered} setRegistered={setRegistered}/>
+//     :
+//       <Outlet />
+//   }</>
+// }
+
+//For this:
+
+// <Outlet />
+
+//To Test things
