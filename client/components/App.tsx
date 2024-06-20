4573b29fb3a399d6f15455316f124caec9af868b
Corina Reitemeyer <corina@corina.org>
2024-06-21 11:27:07 +1200

    Resolve app.tsx

--- client/components/App.tsx
+++ client/components/App.tsx
@@ -2,47 +2,21 @@ import { useEffect, useState } from 'react'
 import { useAuth0 } from '@auth0/auth0-react'
 import { Outlet } from 'react-router-dom'
 import Footer from './Footer.tsx'
-import NavBar from './NavBar.tsx'
+import Header from './Header.tsx'
 import LandingPage from '../pages/LandingPage.tsx'
 import Register from '../pages/Register.tsx'
 import { getUserByAuth } from '../apis/growGrub.ts'
-<<<<<<< HEAD
-import { User } from '../../models/growGrub.ts'
-import Header from './Header.tsx'
-
-const UserContext = createContext<User | undefined>(undefined)
-export const useUser = () => useContext(UserContext)
-
-function App() {
-  const [user, setUser] = useState<User>()
-  const [redirecting, setRedirecting] = useState(true)
-  const { isAuthenticated, getAccessTokenSilently } = useAuth0()
-=======
 
 function App() {
   const [processing, setProcessing] = useState(false)
   const [registered, setRegistered] = useState(false)
-  const {isAuthenticated, getAccessTokenSilently} = useAuth0()
->>>>>>> dev
+  const { isAuthenticated, getAccessTokenSilently } = useAuth0()
 
   useEffect(() => {
     const getUser = async () => {
       setProcessing(true)
       const token = await getAccessTokenSilently()
       try {
-<<<<<<< HEAD
-        const token = await getAccessTokenSilently()
-        const user = await getUserByAuth(token)
-        // console.log(token) use this to grab your token for thunderclient testing
-        setUser(user)
-        setRedirecting(false)
-      } catch (error) {
-        console.error(error)
-      }
-    }
-    getUserByToken()
-  }, [isAuthenticated, getAccessTokenSilently])
-=======
         const result = await getUserByAuth(token)
         if (result.id) setRegistered(true)
       } catch (error) {
@@ -52,38 +26,23 @@ function App() {
       }
     }
     if (isAuthenticated) getUser()
-  },[isAuthenticated, getAccessTokenSilently])
->>>>>>> dev
+  }, [isAuthenticated, getAccessTokenSilently])
 
   return (
     <>
       <div className="app min-w-screen min-h-screen">
-<<<<<<< HEAD
         <Header />
-        {!isAuthenticated || redirecting ? (
+        {!isAuthenticated || processing ? (
           <LandingPage />
         ) : (
           <>
-            {!user ? (
-              <Register />
+            {registered ? (
+              <Outlet />
             ) : (
-              <UserContext.Provider value={user}>
-                <Outlet />
-              </UserContext.Provider>
+              <Register registered={registered} setRegistered={setRegistered} />
             )}
           </>
         )}
-=======
-        <NavBar />
-          {(!isAuthenticated || processing) ?
-            <LandingPage />
-          : <>{registered ? 
-                <Outlet />
-              :
-                <Register registered={registered} setRegistered={setRegistered}/>
-            }</>
-          }
->>>>>>> dev
         <Footer />
       </div>
     </>
@@ -96,7 +55,7 @@ export default App
 
 // {(!isAuthenticated) ?
 //   <LandingPage />
-// : <>{!registered ? 
+// : <>{!registered ?
 //       <Register registered={registered} setRegistered={setRegistered}/>
 //     :
 //       <Outlet />
