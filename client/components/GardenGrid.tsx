7251e85294c819780679aa28e33eb7f3775ed54a
Andrew Molten <andrewmolten23@gmail.com>
2024-06-20 15:08:42 +1200

    figure out how to load a saved grid

--- client/components/GardenGrid.tsx
+++ client/components/GardenGrid.tsx
@@ -15,20 +15,16 @@ function GardenGrid() {
   // send BlockData to the DB
 
   const [layout, setLayout] = useState([
+    { w: 1, h: 16, x: 0, y: 0, i: '1' },
+    { w: 2, h: 9, x: 2, y: 0, i: '2' },
+    { w: 5, h: 1, x: 1, y: 9, i: '3' },
+  ])
+
+  const oldTestData = [
     { w: 1, h: 5, x: 0, y: 0, i: '1' },
     { w: 2, h: 3, x: 2, y: 0, i: '2' },
     { w: 1, h: 2, x: 2, y: 3, i: '3' },
-  ])
-  // [
-  //   { w: 1, h: 5, x: 0, y: 0, i: '1' },
-  //   { w: 2, h: 3, x: 2, y: 0, i: '2' },
-  //   { w: 1, h: 2, x: 2, y: 3, i: '3' }
-  // ]
-  // [
-  //   { i: '1', x: 0, y: 0, w: 4, h: 4 },
-  //   { i: '2', x: 1, y: 6, w: 4, h: 4, minW: 2, maxW: 4 },
-  //   { i: '3', x: 4, y: 0, w: 4, h: 4 },
-  // ]
+  ]
 
   const [blockData, setBlockData] = useState([
     { layoutId: '1', name: 'window garden', sunLight: 5 },
@@ -37,6 +33,7 @@ function GardenGrid() {
   ])
 
   const handleAdd = () => {
+    console.log('handleAdd was triggered')
     const newLayout = [...layout]
     const existingHighestIndex = Number(newLayout[newLayout.length - 1].i)
     const newIdx = String(existingHighestIndex + 1)
@@ -55,13 +52,19 @@ function GardenGrid() {
   }, [layout])
 
   const handleClick = (e) => {
+    console.log('handleClick was triggered')
     console.log(e.target.id.slice(5))
   }
 
   const handleLayoutChange = (newLay) => {
-    setLayout(newLay)
+    if (newLay !== layout) {
+      console.log('handleLayoutChange was triggered')
+      // console.log('The new layout is: ')
+      // console.log(newLay)
+      setLayout(newLay)
+    }
   }
-  const cols = { lg: 12, md: 10, sm: 10, xs: 10, xxs: 2 }
+  const cols = { lg: 12, md: 10, sm: 6, xs: 4, xxs: 3 }
   // console.log(layout)
   return (
     <>
@@ -70,9 +73,10 @@ function GardenGrid() {
         className="layout"
         layout={layout}
         onLayoutChange={handleLayoutChange}
+        breakpoints={{ lg: 1200, md: 996, sm: 768, xs: 480, xxs: 0 }}
         cols={cols}
         rowHeight={30}
-        width={1200}
+        // width={1200}
         margin={[0, 0]}
       >
         {layout.map((block) => (
@@ -81,6 +85,7 @@ function GardenGrid() {
             id={`block${block.i}`}
             onPointerDown={handleClick}
             className="red"
+            data-grid={{ x: block.x, y: block.y, h: block.h, w: block.w }}
           >
             {block.i}
           </button>
