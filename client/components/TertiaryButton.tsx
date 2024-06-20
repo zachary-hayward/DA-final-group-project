41464bcbde584bb7076bdc7989f13328d13e4628
Corina Reitemeyer <corina@corina.org>
2024-06-21 09:47:07 +1200

    Change tertiary button to indigo

--- client/components/TertiaryButton.tsx
+++ client/components/TertiaryButton.tsx
@@ -5,7 +5,7 @@ export default function TertiaryButton() {
       <span className="hidden sm:block">
         <button
           type="button"
-          className="inline-flex items-center px-3 py-2 text-sm font-semibold text-gray-700 hover:text-gray-900"
+          className="inline-flex items-center px-3 py-2 text-sm font-semibold text-indigo-700 hover:text-indigo-900"
         >
           Edit
         </button>
