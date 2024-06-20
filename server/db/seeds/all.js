1bf8d4efce80baae49d107c2207a2c0135ddd349
Zachary Hayward <zmhhayward@gmail.com>
2024-06-20 01:09:17 -0500

    fixed seed

--- server/db/seeds/all.js
+++ server/db/seeds/all.js
@@ -6,7 +6,7 @@ export const seed = async function(knex) {
   // Deletes ALL existing entries
   await knex('users').del()
   await knex('users').insert([
-    { id:1, auth0_id: 'google-oauth2|115329107932736690149' || 'auth0|123', username: 'gardener_john', location: 'New York' },
+    { id:1, auth0_id: 'auth0|123', username: 'gardener_john', location: 'New York' },
     { id:2, auth0_id: 'auth0|456', username: 'plantlover_jane', location: 'California' },
     { id:3, auth0_id: 'auth0|789', username: 'green_thumb_joe', location: 'Texas' }
   ]);
