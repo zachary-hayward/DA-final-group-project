93fcd3acd003f7a89a3b759efa41f7cbd1e86d59
Corina Reitemeyer <corina@corina.org>
2024-06-21 15:00:33 +1200

    Styling correction for footer

--- client/components/Footer.tsx
+++ client/components/Footer.tsx
@@ -3,8 +3,7 @@ export default function Footer() {
     <>
       <footer className="w-full border-t border-gray-300 bg-white py-4">
         <div className="container mx-auto pt-4 text-center">
-          <div className="mx-4 my-4 border-t border-gray-300"></div>
-          <p className="mt-10">&copy; 2024 Grow Grub. All rights reserved.</p>
+          <p className="mt-8">&copy; 2024 Grow Grub. All rights reserved.</p>
           <p className="mt-2">
             Designed with <span className="text-red-500">&hearts;</span> by us
             for you!
