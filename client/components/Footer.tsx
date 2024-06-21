29cd306c8d4f14f1ea9d737c548daf4c98fcb4d2
Corina Reitemeyer <corina@corina.org>
2024-06-21 15:23:41 +1200

    Lighter color line and spacing fix for footer

--- client/components/Footer.tsx
+++ client/components/Footer.tsx
@@ -1,10 +1,12 @@
 export default function Footer() {
   return (
     <>
-      <footer className="w-full border-t border-gray-300 bg-white py-4">
+      <footer className="w-full border-t border-gray-200 bg-white py-4">
         <div className="container mx-auto pt-4 text-center">
-          <p className="mt-8">&copy; 2024 Grow Grub. All rights reserved.</p>
-          <p className="mt-2">
+          <p className="mb-4 mt-4">
+            &copy; 2024 Grow Grub. All rights reserved.
+          </p>
+          <p className="mb-5 mt-2">
             Designed with <span className="text-red-500">&hearts;</span> by us
             for you!
           </p>
