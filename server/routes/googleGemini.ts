9a5e8b1091cefc4b013dc4c9abd2d8d3fc59c68a
Sarah Situ <163368313+sarah-situ@users.noreply.github.com>
2024-06-22 13:48:51 +1200

    add query key and new table for plant care data

--- server/routes/googleGemini.ts
+++ server/routes/googleGemini.ts
@@ -19,7 +19,8 @@ router.get('/', checkJwt, async (req: JwtRequest, res) => {
   }
 
   try {
-    const prompt = `give me json data with whitespacing between each word about plant care information about strawberries that contains plant care information, watering with one of three properties: low, medium or high, time should be the season with the seed planting months in New Zealand, spacing in metres, in the form of {
+    const vege1 = req.query.vege
+    const prompt = `give me json data with whitespacing between each word about plant care information about ${vege1} that contains plant care information, watering with one of three properties: low, medium or high, time should be the season with the seed planting months in New Zealand, spacing in metres, in the form of {
 "plantCareData": [
   {
     "plantName": str,
@@ -35,18 +36,19 @@ router.get('/', checkJwt, async (req: JwtRequest, res) => {
         "diseases": str
     },
     "plantingTime": {
-        "indoors": "Start seeds indoors 6-8 weeks before the last frost date in your area.",
-        "outdoors": "Transplant seedlings outdoors after all danger of frost has passed and soil temperatures are consistently above 55°F (13°C).",,
+        "indoorsPlantingTime": "Start seeds indoors 6-8 weeks before the last frost date in your area.",
+        "outdoorsPlantingTime": "Transplant seedlings outdoors after all danger of frost has passed and soil temperatures are consistently above 55°F (13°C).",,
         "spacing": str,
-        "time": [str],
+        "plantingTime": [str],
     },
     "harvesting": {
-        "time": str,
-        "tips": str
+        "havestingTime": str,
+        "harvestingTips": str
     }
 }
   ]
 } and for each property, keep the strings short please and with spacing between words`
+
     const genAI = new GoogleGenerativeAI(geminiApiKey)
     const model = genAI.getGenerativeModel({ model: 'gemini-1.5-flash' })
     const result = await model.generateContent(prompt)
@@ -74,72 +76,74 @@ router.get('/', checkJwt, async (req: JwtRequest, res) => {
   }
 })
 
-router.post('/', checkJwt, async (req: JwtRequest, res) => {
-  console.log(111)
-  const authoId = req.auth?.sub
-  if (geminiApiKey === undefined) {
-    throw new Error('Missing google gemini key environment variable')
-  }
-  if (!authoId) {
-    console.log('No authoId')
-    return res.status(401).send('Unauthorized')
-  }
+// router.post('/', checkJwt, async (req: JwtRequest, res) => {
+//   console.log(111)
+//   const authoId = req.auth?.sub
+//   if (geminiApiKey === undefined) {
+//     throw new Error('Missing google gemini key environment variable')
+//   }
+//   if (!authoId) {
+//     console.log('No authoId')
+//     return res.status(401).send('Unauthorized')
+//   }
 
-  try {
-    const vege = req.body.prompt
-    const prompt = `give me json data with whitespacing between each word about plant care information about strawberries that contains plant care information, watering with one of three properties: low, medium or high, time should be the season with the seed planting months in New Zealand, spacing in metres, in the form of {
-"plantCareData": [
-  {
-    "plantName": str,
-    "scientificName": str,
-    "description": " ",
-    "careInstructions": {
-        "soil": str,
-        "sunlight": str
-        "watering": str,
-        "fertilization": str,
-        "pruning": str,
-        "pests": str,
-        "diseases": str
-    },
-    "plantingTime": {
-        "indoors": "Start seeds indoors 6-8 weeks before the last frost date in your area.",
-        "outdoors": "Transplant seedlings outdoors after all danger of frost has passed and soil temperatures are consistently above 55°F (13°C).",,
-        "spacing": str,
-        "time": [str],
-    },
-    "harvesting": {
-        "time": str,
-        "tips": str
-    }
-}
-  ]
-} and for each property, keep the strings short please and with spacing between words`
-    const genAI = new GoogleGenerativeAI(geminiApiKey)
-    const model = genAI.getGenerativeModel({ model: 'gemini-1.5-flash' })
-    const result = await model.generateContent(prompt)
-    const response = await result.response
-    // console.log(JSON.stringify(response.candidates[0].content.parts[0].text))
+//   try {
+//     const { vege } = req.body
+//     console.log(req.body)
+//     const prompt = `give me json data with whitespacing between each word about plant care information about ${vege} that contains plant care information, watering with one of three properties: low, medium or high, time should be the season with the seed planting months in New Zealand, spacing in metres, in the form of {
+// "plantCareData": [
+//   {
+//     "plantName": str,
+//     "scientificName": str,
+//     "description": " ",
+//     "careInstructions": {
+//         "soil": str,
+//         "sunlight": str
+//         "watering": str,
+//         "fertilization": str,
+//         "pruning": str,
+//         "pests": str,
+//         "diseases": str
+//     },
+//     "plantingTime": {
+//         "indoors": "Start seeds indoors 6-8 weeks before the last frost date in your area.",
+//         "outdoors": "Transplant seedlings outdoors after all danger of frost has passed and soil temperatures are consistently above 55°F (13°C).",,
+//         "spacing": str,
+//         "time": [str],
+//     },
+//     "harvesting": {
+//         "time": str,
+//         "tips": str
+//     }
+// }
+//   ]
+// } and for each property, keep the strings short please and with spacing between words`
+//     const genAI = new GoogleGenerativeAI(geminiApiKey)
+//     const model = genAI.getGenerativeModel({ model: 'gemini-1.5-flash' })
+//     const result = await model.generateContent(prompt)
+//     const response = await result.response
+//     // console.log(JSON.stringify(response.candidates[0].content.parts[0].text))
 
-    const reg = new RegExp(`\\n`, `g`)
-    const text1 = response.candidates[0].content.parts[0].text
+//     const reg = new RegExp(`\\n`, `g`)
+//     const text1 = response.candidates[0].content.parts[0].text
+//     console.log(text1)
 
-    const newStr = text1
-      .replace(reg, '')
-      .replace(new RegExp('```json', 'g'), ' ')
-      .replace(new RegExp('```', 'g'), ' ')
-      .replace(new RegExp(`\\s`, 'g'), ' ')
+//     // const newStr = text1
+//     //   .replace(reg, '')
+//     //   .replace(new RegExp('```json', 'g'), ' ')
+//     //   .replace(new RegExp('```', 'g'), ' ')
+//     //   .replace(new RegExp(`\\s`, 'g'), ' ')
 
-    res.json(JSON.parse(newStr))
-    // console.log(JSON.stringify(response.candidates))
-  } catch (err) {
-    if (err instanceof Error) {
-      console.log(err.message)
-      res.status(500).send((err as Error).message)
-    } else {
-      res.status(500).send('Something went wrong')
-    }
-  }
-})
+//     // res.json(JSON.parse(newStr))
+//     // console.log(JSON.stringify(response.candidates))
+//   } catch (err) {
+//     if (err instanceof Error) {
+//       console.log(err.message)
+//       res.status(500).send((err as Error).message)
+//     } else {
+//       res.status(500).send('Something went wrong')
+//     }
+//   }
+// })
 
 export default router
