0251b91b2a85e1571d4096adacdfbafb84d49659
Andrew Molten <andrewmolten23@gmail.com>
2024-06-22 17:23:14 +1200

    add props to button components

--- client/components/SecondaryButton.tsx
+++ client/components/SecondaryButton.tsx
@@ -1,4 +1,9 @@
-export default function SecondaryButton() {
+interface Props {
+  onClick: (event: React.MouseEvent) => void
+  children: React.ReactNode
+}
+
+export default function SecondaryButton({ onClick, children }: Props) {
   // Tailwind Secondary Button
   return (
     <>
@@ -6,6 +11,7 @@ export default function SecondaryButton() {
         <button
           type="button"
           className="inline-flex items-center rounded-md bg-white px-3 py-2 text-sm font-semibold text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 hover:bg-gray-50"
+          onClick={onClick}
         >
           <svg
             className="-ml-0.5 mr-1.5 h-5 w-5 text-gray-400"
@@ -15,7 +21,7 @@ export default function SecondaryButton() {
           >
             <path d="M2.695 14.763l-1.262 3.154a.5.5 0 00.65.65l3.155-1.262a4 4 0 001.343-.885L17.5 5.5a2.121 2.121 0 00-3-3L3.58 13.42a4 4 0 00-.885 1.343z" />
           </svg>
-          Edit
+          {children}
         </button>
       </span>
     </>
