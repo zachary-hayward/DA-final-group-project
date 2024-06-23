f345d575086711614ce352087c23c7d426ab1c6b
Corina Reitemeyer <corina@corina.org>
2024-06-24 10:29:34 +1200

    Add Plants table to the plants page

--- client/pages/MyPlants.tsx
+++ client/pages/MyPlants.tsx
@@ -6,7 +6,7 @@ export default function TaskPage() {
       {/* Page Banner - hard coded - currently WIP of componentising it */}
       <div className="banner-container">
         <div className="mx-auto max-w-7xl text-center">
-          <h2 className="banner-title">My Tasks</h2>
+          <h2 className="banner-title">My Plants</h2>
         </div>
       </div>
       <div className="my-20">
