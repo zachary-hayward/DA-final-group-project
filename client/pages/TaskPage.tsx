ba568f9230ffc82c8007ab7c9b569b2bb4336634
Corina Reitemeyer <corina@corina.org>
2024-06-23 20:24:12 +1200

    Add Task table to page

--- client/pages/TaskPage.tsx
+++ client/pages/TaskPage.tsx
@@ -1,3 +1,5 @@
+import TasksTable from '../components/TasksTable'
+
 export default function TaskPage() {
   return (
     <>
@@ -6,6 +8,16 @@ export default function TaskPage() {
         <div className="mx-auto max-w-7xl text-center">
           <h2 className="banner-title">My Tasks</h2>
         </div>
+      </div>
+      <div className="my-20">
+        <TasksTable
+          id={0}
+          plantName={''}
+          plantImage={''}
+          plantedDate={''}
+          taskType={''}
+          lastPerformed={''}
+        />
       </div>
     </>
   )
