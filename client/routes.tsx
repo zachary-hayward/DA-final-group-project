737d7090a68c0ecaf3e9f982ea80212d6b3837e0
Corina Reitemeyer <corina@corina.org>
2024-06-23 20:26:00 +1200

    Add front-end route for Taskpage

--- client/routes.tsx
+++ client/routes.tsx
@@ -4,6 +4,7 @@ import Home from './pages/Home.tsx'
 
 import SinglePlant from './pages/SinglePlant.tsx'
 import { GardenView } from './pages/GardenView.tsx'
+import TaskPage from './pages/TaskPage.tsx'
 
 export default createRoutesFromElements(
   <Route path="/" element={<App />}>
@@ -12,5 +13,6 @@ export default createRoutesFromElements(
     {/* '/my-plants' should link to the plants list of an exisiting garden */}
     <Route path="/my-plants/:id" element={<SinglePlant />} />
     <Route path="/my-garden" element={<GardenView />} />
+    <Route path="/my-tasks" element={<TaskPage />} />
   </Route>,
 )
