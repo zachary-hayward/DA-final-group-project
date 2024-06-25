fad03d48d9a0ab6a11232c096cbea20ea68fb588
Andrew Molten <andrewmolten23@gmail.com>
2024-06-25 14:27:19 +1200

    remove comments

--- client/components/GardenButton.tsx
+++ client/components/GardenButton.tsx
@@ -22,7 +22,3 @@ function GardenButton({ garden, switchSelectedGarden }: Props) {
 }
 
 export default GardenButton
-
-{
-  /* <div class="mx-auto mt-10 flex max-w-7xl justify-center p-8"><a class="mx-4 flex-1" href="/my-garden"><div class="rounded-lg bg-white p-6 shadow-md transition duration-300 hover:shadow-lg"><div class="mb-4 flex items-center justify-center"><div class="mr-4 style= w-20 h-20"><img src="/images/flat-icons/spinach.png" alt="Spinach"></div><div>My Garden</div></div></div></a><a class="mx-4 flex-1" href="/my-plants"><div class="rounded-lg bg-white p-6 shadow-md transition duration-300 hover:shadow-lg"><div class="mb-4 flex items-center justify-center"><div class="mr-4 style= w-20 h-20"><img src="/images/flat-icons/carrot.png" alt="Carrot"></div><div>My Plants</div></div></div></a><a class="mx-4 flex-1" href="/my-tasks"><div class="rounded-lg bg-white p-6 shadow-md transition duration-300 hover:shadow-lg"><div class="mb-4 flex items-center justify-center"><div class="mr-4 style= w-20 h-20"><img src="/images/flat-icons/watering-can.png" alt="Watering Can"></div><div>My Tasks</div></div></div></a></div> */
-}
